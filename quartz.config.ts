17b580a941e65ad92fcd07de89de3953695670ab
dndngael <sushi.fsh1@gmail.com>
2025-11-21 01:16:36 -0600

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -68,7 +68,7 @@ const config: QuartzConfig = {
       }),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
-      Plugin.stripSecrets(),
+      Plugin.StripSecrets(),
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
