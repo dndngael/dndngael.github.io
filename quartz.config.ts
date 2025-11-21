e6eaa566aec803f0ce985cdcb9ac27830bfcc7bd
dndngael <sushi.fsh1@gmail.com>
2025-11-21 01:57:53 -0600

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,6 +1,5 @@
 import { QuartzConfig } from "./quartz/cfg"
 import * as Plugin from "./quartz/plugins"
-import { StripDmCallouts } from "./quartz/plugins/stripDmCallouts"
 
 /**
  * Quartz 4 Configuration
