e483eee625fdb49f9fa325a0fc70ce8d2634b160
dndngael <sushi.fsh1@gmail.com>
2025-11-26 23:27:46 -0600

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -23,7 +23,7 @@ export const defaultContentPageLayout: PageLayout = {
     //Component.ArticleTitle(),
     //Component.ContentMeta(),
     //Component.TagList(),
-    //Component.MobileOnly(Component.Infobox()),
+    Component.MobileOnly(Component.Infobox()),
   ],
   left: [
     Component.PageTitle(),
@@ -41,8 +41,8 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph(),
     Component.DesktopOnly(Component.Infobox()),
+    Component.Graph(),
     Component.Backlinks(),
   ],
 }
