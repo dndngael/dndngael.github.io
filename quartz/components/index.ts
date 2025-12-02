b8f14914b4ef53f5bb4e89e0c947705fbd414d84
dndngael <sushi.fsh1@gmail.com>
2025-12-01 23:50:20 -0600

    Update index.ts

--- quartz/components/index.ts
+++ quartz/components/index.ts
@@ -23,7 +23,7 @@ import Breadcrumbs from "./Breadcrumbs"
 import Comments from "./Comments"
 import Flex from "./Flex"
 import ConditionalRender from "./ConditionalRender"
-import Infobox from "./Infobox"
+//import Infobox from "./Infobox"
 
 export {
   ArticleTitle,
@@ -51,5 +51,5 @@ export {
   Comments,
   Flex,
   ConditionalRender,
-  Infobox,
+  //Infobox,
 }
