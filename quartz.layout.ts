a9118c8dac51165e48075e8e7e6891224393a2e1
dndngael <sushi.fsh1@gmail.com>
2025-12-02 00:14:57 -0600

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -15,8 +15,8 @@ export const sharedPageComponents: SharedLayout = {
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
-  /*
   beforeBody: [
+    /*
     Component.ConditionalRender({
       //component: Component.Breadcrumbs(),
       //component: Component.Graph(),
@@ -26,8 +26,10 @@ export const defaultContentPageLayout: PageLayout = {
     //Component.ContentMeta(),
     //Component.TagList(),
     //Component.MobileOnly(Component.Infobox()),
-  ],
+  */
+    ],
   left: [
+    /*
     //Component.PageTitle(),
     //Component.MobileOnly(Component.Spacer()),
     Component.Flex({
@@ -41,20 +43,20 @@ export const defaultContentPageLayout: PageLayout = {
       ],
     }),
     //Component.Explorer(),
+  */
   ],
   right: [
     //Component.Infobox(),
     //Component.DesktopOnly(Component.Infobox()),
     //Component.Backlinks(),
   ],
-  */
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  /*
-  //beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [/*Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()*/],
   left: [
+    /*
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Flex({
@@ -67,7 +69,7 @@ export const defaultListPageLayout: PageLayout = {
       ],
     }),
     Component.Explorer(),
+    */
   ],
   right: [],
-  */
 }
